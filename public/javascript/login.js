8f685c87622f4ceecfa6e468d62995c6d847f1d6
Amy Gardiner <amymgardiner@gmail.com>
2022-08-09 16:12:59 -0500

    created dashboard page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
@@ -46,12 +46,16 @@ async function signupFormHandler(event) {
 
     // check the response status
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
   }
 }
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
 
 document
   .querySelector('.signup-form')
