ce8b41e985a26bc83ffa562a39d747267a405ef3
Amy Gardiner <amymgardiner@gmail.com>
2022-08-10 20:42:28 -0500

    tweaked a bit of code to account for the auth utility

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -103,19 +103,17 @@ router.post('/', withAuth, (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', withAuth, (req, res) => {
-  // make sure the session exists first
-  if (req.session) {
-    // pass session id along with all destructured properties on req.body
-    Post.upvote(
-      { ...req.body, user_id: req.session.user_id },
-      { Vote, Comment, User }
-    )
-      .then((updatedVoteData) => res.json(updatedVoteData))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  }
+  // pass session id along with all destructured properties on req.body
+  // custom static method created in models/Post.js
+  Post.upvote(
+    { ...req.body, user_id: req.session.user_id },
+    { Vote, Comment, User }
+  )
+    .then((updatedVoteData) => res.json(updatedVoteData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', withAuth, (req, res) => {
